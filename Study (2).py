daf2a3a0e239aa7f95bf0bd89843e7bdf8b37696
mvirag2000 <mark@viragconsulting.com>
2016-11-17 13:13:45 -0500

    Version from the blog post
    Added datareader for stock symbols

---
+++ Study (2).py
@@ -0,0 +1,71 @@
+import pandas as pd
+from bokeh.io import output_file, show
+from bokeh.layouts import column, row
+from bokeh.plotting import figure
+from bokeh.models.widgets.inputs import Slider
+import numpy as np
+import virag as v
+from pandas.io.pytables import IndexCol
+from pandas.io.tests.parser import index_col
+import pandas.io.data as web
+import datetime
+
+start = datetime.datetime(2008,1,1)
+end = datetime.datetime(2008,12,31)
+stocks = ['SPY', 'XLI', 'XLB', 'XLE', 'XLU', 'XLF', 'XLV', 'XLY', 'XLP', 'XLK']
+g=web.DataReader(stocks, 'yahoo', start, end)
+price = g['Close']
+#price = pd.read_csv('F:\Stock Market\Ziggy Lines\Correlation\Including SPY.csv',encoding = 'mbcs',index_col=0)
+#v.print_stats(price)
+
+w = 30 #rolling correlation window
+s = 5 #SMA smoothing interval 
+lo = 60 #low correlation threshold 
+
+spy = price['SPY'].copy()
+price = price.drop('SPY', 1)
+
+delta = pd.DataFrame(index=price.index, columns=price.columns)
+for c in price.columns:
+    delta[c] = price[c].pct_change()
+days = delta.shape[0]
+symbols = delta.shape[1]
+i = 0
+corr_tab = np.empty(shape=[days, symbols**2])
+for c1 in delta.columns:
+    for c2 in delta.columns:
+        corr_tab[:,i] = delta[c1].rolling(window=w).corr(delta[c2])
+        i += 1 
+avg_corr = pd.DataFrame(index=delta.index, columns=['Correlation'])
+avg_corr['Correlation'] = 100 * (corr_tab.sum(axis=1) - symbols) / 2 / v.pairs(symbols)
+avg_corr['SPY'] = spy
+avg_corr['Date'] = pd.to_datetime(avg_corr.index)
+avg_corr['Smooth SPY'] = avg_corr['SPY'].rolling(window=s).mean()
+avg_corr['Smooth Corr'] = avg_corr['Correlation'].rolling(window=s).mean()
+avg_corr = avg_corr.iloc[w+s:,:] #truncate the wasted w+s days
+avg_corr.loc[avg_corr['Correlation'] <= lo, 'Low'] = True 
+
+box = avg_corr.loc[:,['Date', 'Low']]
+box = box[box['Low'] == True] 
+top = avg_corr['SPY'].max()
+bottom = avg_corr['SPY'].min()
+box['Top'] = top
+box['Bottom'] = bottom
+box['Right'] = box['Date'] + pd.DateOffset(2)
+
+s1 = figure(plot_width=940, plot_height=420, title='SPY', x_axis_type='datetime')
+
+#Examine deltas directly
+#for i in delta.columns:
+#    delta[i] = delta[i].rolling(window=s).mean()
+#delta = delta.iloc[w+s:,:]
+#s1.multi_line(xs=[delta.index.values]*9, ys=[delta[name].values for name in delta], 
+#    color=['red','firebrick','orange','yellow', 'blue','green','gray','indigo','violet'])
+s1.line(avg_corr['Date'], avg_corr['Smooth SPY'], line_color='red')
+s1.quad(left=box['Date'], right=box['Right'], top=box['Top'], bottom=box['Bottom'],
+        color='grey', alpha=0.2)
+
+s2 = figure(plot_width=940, plot_height=150, title='Correlation', x_axis_type='datetime')
+s2.line(avg_corr['Date'], avg_corr['Smooth Corr'], line_color='blue')
+
+show(column(s1, s2))
